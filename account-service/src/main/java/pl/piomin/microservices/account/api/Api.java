5f8edd5673e853c0fdd00ffdf42b35676207a43f
tuanhiep225 <tuanhiep225@gmail.com>
2018-07-19 00:27:12 +0700

    tesst

--- account-service/src/main/java/pl/piomin/microservices/account/api/Api.java
+++ account-service/src/main/java/pl/piomin/microservices/account/api/Api.java
@@ -20,9 +20,9 @@ public class Api {
 	
 	public Api() {
 		accounts = new ArrayList<>();
-		accounts.add(new Account(1, 1, "11111154"));
-		accounts.add(new Account(2, 2, "22225252"));
-		accounts.add(new Account(3, 3, "33434333"));
+		accounts.add(new Account(1, 1, "111111"));
+		accounts.add(new Account(2, 2, "222222"));
+		accounts.add(new Account(3, 3, "333333"));
 		accounts.add(new Account(4, 4, "444444"));
 		accounts.add(new Account(5, 1, "555555"));
 		accounts.add(new Account(6, 2, "666666"));
